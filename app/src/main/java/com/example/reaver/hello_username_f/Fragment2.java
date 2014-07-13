1e4d8ef53e3f52422df0ede27c22bda27fe8d30a
Reaver <ellis.neo.reg@gmail.com>
2014-07-13 20:07:13 +0300

    Add fragments, backgrounds, icons

---
+++ app/src/main/java/com/example/reaver/hello_username_f/Fragment2.java
@@ -0,0 +1,22 @@
+package com.example.reaver.hello_username_f;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Reaver on 13.07.2014.
+ */
+public class Fragment2 extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
